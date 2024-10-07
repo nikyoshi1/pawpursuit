b3519bc37346daee876b38e18df9543c6a71ce90
ct6 <cyantechsix@gmail.com>
2024-10-07 00:39:06 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("generateQR").addEventListener("click", function() {
+    // QR Code logic goes here
+    document.getElementById("qrCode").innerHTML = "QR Code will be generated here.";
+});
